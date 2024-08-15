688634f70b9b81e2c54f9e2167605595beb06fc0
dsyang <dsyang.tech@gmail.com>
2024-08-15 21:56:16 +0900

    label 태그를 자동으로 만들어지도록 변경, 단위를 입력하고 단위에 맞게 변환하도록 추가

--- lib/coding_training_elixir_web/live/chapter7/chapter7_live.ex
+++ lib/coding_training_elixir_web/live/chapter7/chapter7_live.ex
@@ -1,37 +1,45 @@
 defmodule CodingTrainingElixirWeb.Chapter7Live do
   use CodingTrainingElixirWeb, :live_view
   @convert_value 0.09290304
+  @feet "feet"
+  @meter "meter"
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, result: "결과가 여기에 나타납니다")
+    socket = assign(socket, result: "결과가 여기에 나타납니다", unit_options: [피트: @feet, 미터: @meter])
     {:ok, socket}
   end
 
-  def handle_event("input-change", %{"length" => length, "width" => width}, socket) do
+  def handle_event(
+        "input-change",
+        %{"length" => length, "width" => width, "unit" => unit},
+        socket
+      ) do
     case valid(length, width) do
       {:ok, {l, w}} ->
-        sqfeet = l * w
-        sqmeter = sqfeet_to_sqmeter(sqfeet)
-        result = print(l, w, sqfeet, sqmeter)
+        area = l * w
+        {counter_unit, counter_area} = calculate_counter_area(unit, area)
+        result = print(l, w, area, counter_area, unit, counter_unit)
         socket = assign(socket, result: result)
         {:noreply, socket}
 
       {:error, msg} ->
-        IO.inspect(msg)
         socket = assign(socket, result: msg)
         {:noreply, socket}
     end
   end
 
-  def print(length, width, sqfeet, sqmeter) do
-    "당신이 입력한 방의 길이는 #{length} 피트, #{width} 피트 입니다.<br/> 
-    면적은 
-    #{sqfeet} 제곱피트<br/>
-    #{sqmeter} 제곱미터 입니다."
+  def calculate_counter_area(@feet, area) do
+    {@meter, area * @convert_value}
   end
 
-  def sqfeet_to_sqmeter(sqfeet) do
-    sqfeet * @convert_value
+  def calculate_counter_area(@meter, area) do
+    {@feet, area / @convert_value}
+  end
+
+  def print(length, width, area, counter_area, unit, counter_unit) do
+    "당신이 입력한 방의 길이는 #{length} #{unit}, #{width} #{unit} 입니다.<br/> 
+    면적은 #{area} 제곱#{unit}<br/>
+    #{counter_area} 제곱#{counter_unit} 입니다."
   end
 
   def valid(length, width) do
