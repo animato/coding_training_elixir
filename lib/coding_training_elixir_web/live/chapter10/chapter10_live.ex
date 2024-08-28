1938756897ba7445f0dc8a5b09c6b66ed0e8c2d9
dsyang <dsyang.tech@gmail.com>
2024-08-28 20:52:36 +0900

    상품 수 및 입력 폼을 서버에서 만들도록 변경

--- lib/coding_training_elixir_web/live/chapter10/chapter10_live.ex
+++ lib/coding_training_elixir_web/live/chapter10/chapter10_live.ex
@@ -1,22 +1,25 @@
 defmodule CodingTrainingElixirWeb.Chapter10Live do
   use CodingTrainingElixirWeb, :live_view
   @tax_rate 5.5
+  @init_item_count 3
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, result: %{subtotal: 0, tax: 0, total: 0})
+    item = %{price: nil, quantity: nil}
+    items = Enum.map(1..@init_item_count, fn _ -> Map.new(item) end)
+    socket = assign(socket, items: items, result: %{subtotal: 0, tax: 0, total: 0})
     {:ok, socket}
   end
 
   def handle_event(
         "input-submit",
-        %{
-          "item1" => item1,
-          "item2" => item2,
-          "item3" => item3
-        },
+        %{"item" => items},
         socket
       ) do
-    list = [item1, item2, item3]
+    # IO.inspect(items)
+
+    list = Enum.map(items, fn {_, value} -> value end)
+
+    # IO.inspect(list)
 
     subtotal =
       Enum.map(list, fn x -> String.to_integer(x["price"]) * String.to_integer(x["quantity"]) end)
