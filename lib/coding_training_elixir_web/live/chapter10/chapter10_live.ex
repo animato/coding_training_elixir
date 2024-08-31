2124c37074cf86137c335c0ccff8a2d0be8c407c
dsyang <dsyang.tech@gmail.com>
2024-08-31 21:48:20 +0900

    input 구조 변경 및 validation 추가

--- lib/coding_training_elixir_web/live/chapter10/chapter10_live.ex
+++ lib/coding_training_elixir_web/live/chapter10/chapter10_live.ex
@@ -4,24 +4,66 @@ defmodule CodingTrainingElixirWeb.Chapter10Live do
   @init_item_count 3
 
   def mount(_params, _session, socket) do
-    item = %{price: nil, quantity: nil}
-    items = Enum.map(1..@init_item_count, fn _ -> Map.new(item) end)
+    item_price = %{price_or_quantity: "price", value: nil, errors: []}
+    item_quantity = %{price_or_quantity: "quantity", value: nil, errors: []}
+
+    items =
+      Enum.flat_map(1..@init_item_count, fn _ ->
+        [
+          Map.new(item_price),
+          Map.new(item_quantity)
+        ]
+      end)
+
     socket = assign(socket, items: items, result: %{subtotal: 0, tax: 0, total: 0})
+
     {:ok, socket}
   end
 
   def handle_event("add", _, socket) do
-    items = socket.assigns.items ++ [%{price: nil, quantity: nil}]
+    items = socket.assigns.items ++ [%{price_or_quantity: nil, value: nil, errors: []}]
     socket = assign(socket, items: items)
     {:noreply, socket}
   end
 
   def handle_event("validate", %{"item" => items}, socket) do
     list =
-      Enum.map(items, fn {_, value} -> %{price: value["price"], quantity: value["quantity"]} end)
+      Enum.map(items, fn {_, value} ->
+        case value do
+          %{"price" => v} ->
+            validate_field("price", v)
+
+          %{"quantity" => v} ->
+            validate_field("quantity", v)
+        end
+      end)
 
     socket = assign(socket, items: list)
     {:noreply, socket}
+  end
+
+  defp validate_field(field_type, value) do
+    case valid(value) do
+      {:ok, _} ->
+        %{price_or_quantity: field_type, value: value, errors: []}
+
+      {:error, msg} ->
+        %{price_or_quantity: field_type, value: value, errors: [msg]}
+    end
+  end
+
+  def valid(string) when string != nil do
+    with {number, ""} <- Integer.parse(string),
+         true <- number > 0 do
+      {:ok, number}
+    else
+      :error -> {:error, "숫자가 아닌 값이 입력되었습니다."}
+      _ -> {:error, "0 또는 음수 값 입력되었습니다."}
+    end
+  end
+
+  def valid(string) when string == nil do
+    {:error, "값 입력 안됨"}
   end
 
   def handle_event(
