9cd6729568d5c91b03a12fb867be211091040163
dsyang <dsyang.tech@gmail.com>
2024-09-07 21:54:24 +0900

    값 검증 추가

--- lib/coding_training_elixir_web/live/chapter11/chapter11_live.ex
+++ lib/coding_training_elixir_web/live/chapter11/chapter11_live.ex
@@ -3,23 +3,49 @@ defmodule CodingTrainingElixirWeb.Chapter11Live do
   @rate_to 100
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, valid: false, result: %{amount_from: 0, rate_from: 0, amount_to: 0})
+    socket =
+      assign(socket,
+        valid: false,
+        amount_from_error: [],
+        rate_from_error: [],
+        result: %{amount_from: 0, rate_from: 0, amount_to: 0}
+      )
 
     {:ok, socket}
   end
 
-  # def handle_event("validate", %{"item" => items}, socket) do
-  #   socket = assign(socket, items: list, valid: valid)
-  #   {:noreply, socket}
-  # end
+  def handle_event("validate", %{"amount_from" => amount_from, "rate_from" => rate_from}, socket) do
+    amount_from_error =
+      case Integer.parse(amount_from) do
+        {number, ""} when number > 0 -> []
+        {_, ""} -> ["0 또는 음수 값이 입력되었습니다."]
+        _ -> ["숫자가 아닌 값이 입력되었습니다."]
+      end
+
+    rate_from_error =
+      case Float.parse(rate_from) do
+        {number, ""} when number > 0 -> []
+        {_, ""} -> ["0 또는 음수 값이 입력되었습니다."]
+        _ -> ["실수가 아닌 값이 입력되었습니다."]
+      end
+
+    socket =
+      assign(socket,
+        valid: rate_from_error == [] and rate_from_error == [],
+        amount_from_error: amount_from_error,
+        rate_from_error: rate_from_error
+      )
+
+    {:noreply, socket}
+  end
 
   def handle_event(
         "input-submit",
         %{"amount_from" => amount_from, "rate_from" => rate_from},
         socket
       ) do
-    {a, "" } = Integer.parse(amount_from)
-    {r, "" } = Float.parse(rate_from)
+    {a, ""} = Integer.parse(amount_from)
+    {r, ""} = Float.parse(rate_from)
     amount_to = calculate(a, r) |> Float.ceil(2)
 
     socket = assign(socket, :result, %{amount_from: a, rate_from: r, amount_to: amount_to})
@@ -30,24 +56,4 @@ defmodule CodingTrainingElixirWeb.Chapter11Live do
   def calculate(amount_from, rate_from) do
     amount_from * rate_from / @rate_to
   end
-
-  def error_message(price) do
-    case valid_integer(price) do
-      {:error, msg} -> [msg]
-      _ -> []
-    end
-  end
-
-  def valid_integer(nil) do
-    {:error, "값이 입력되지 않았습니다."}
-  end
-
-  def valid_integer(string) do
-    case Integer.parse(string) do
-      {number, ""} when number > 0 -> {:ok, number}
-      {_, ""} -> {:error, "0 또는 음수 값이 입력되었습니다."}
-      _ -> {:error, "숫자가 아닌 값이 입력되었습니다."}
-    end
-  end
-
 end
