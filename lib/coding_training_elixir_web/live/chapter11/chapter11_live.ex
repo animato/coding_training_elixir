32123c5a00f03dcc4d4c594de03f3e3930798b9c
dsyang <dsyang.tech@gmail.com>
2024-09-09 22:38:40 +0900

    환율표 적용

--- lib/coding_training_elixir_web/live/chapter11/chapter11_live.ex
+++ lib/coding_training_elixir_web/live/chapter11/chapter11_live.ex
@@ -1,11 +1,13 @@
 defmodule CodingTrainingElixirWeb.Chapter11Live do
   use CodingTrainingElixirWeb, :live_view
   @rate_to 100
+  @country ["USD", "KRW", "EUR"]
 
   def mount(_params, _session, socket) do
     socket =
       assign(socket,
         valid: false,
+        country: @country,
         amount_from_error: [],
         rate_from_error: [],
         result: %{amount_from: 0, rate_from: 0, amount_to: 0}
@@ -14,7 +16,7 @@ defmodule CodingTrainingElixirWeb.Chapter11Live do
     {:ok, socket}
   end
 
-  def handle_event("validate", %{"amount_from" => amount_from, "rate_from" => rate_from}, socket) do
+  def handle_event("validate", %{"amount_from" => amount_from, "country" => country}, socket) do
     amount_from_error =
       case Integer.parse(amount_from) do
         {number, ""} when number > 0 -> []
@@ -22,18 +24,11 @@ defmodule CodingTrainingElixirWeb.Chapter11Live do
         _ -> ["숫자가 아닌 값이 입력되었습니다."]
       end
 
-    rate_from_error =
-      case Float.parse(rate_from) do
-        {number, ""} when number > 0 -> []
-        {_, ""} -> ["0 또는 음수 값이 입력되었습니다."]
-        _ -> ["실수가 아닌 값이 입력되었습니다."]
-      end
-
     socket =
       assign(socket,
-        valid: rate_from_error == [] and rate_from_error == [],
-        amount_from_error: amount_from_error,
-        rate_from_error: rate_from_error
+        valid: amount_from_error == [],
+        amount_from_error: amount_from_error
+        # rate_from_error: rate_from_error
       )
 
     {:noreply, socket}
@@ -41,14 +36,20 @@ defmodule CodingTrainingElixirWeb.Chapter11Live do
 
   def handle_event(
         "input-submit",
-        %{"amount_from" => amount_from, "rate_from" => rate_from},
+        %{"amount_from" => amount_from, "country" => country},
         socket
       ) do
     {a, ""} = Integer.parse(amount_from)
-    {r, ""} = Float.parse(rate_from)
-    amount_to = calculate(a, r) |> Float.ceil(2)
 
-    socket = assign(socket, :result, %{amount_from: a, rate_from: r, amount_to: amount_to})
+    rate_table = %{"USD" => 137.51, "EUR" => 100, "KRW" => 148_311}
+    rate_from = rate_table[country]
+
+    IO.inspect(rate_from)
+
+    amount_to = calculate(a, rate_from) |> Float.ceil(2)
+
+    socket =
+      assign(socket, :result, %{amount_from: a, rate_from: rate_from, amount_to: amount_to})
 
     {:noreply, socket}
   end
