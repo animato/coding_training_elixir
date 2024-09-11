6e7b5c72c252ff0043417716695713df5a4450c1
dsyang <dsyang.tech@gmail.com>
2024-09-11 22:26:49 +0900

    연습문제5 코드 개선

--- lib/coding_training_elixir_web/live/chapter5/chapter5_live.ex
+++ lib/coding_training_elixir_web/live/chapter5/chapter5_live.ex
@@ -2,24 +2,62 @@ defmodule CodingTrainingElixirWeb.Chapter5Live do
   use CodingTrainingElixirWeb, :live_view
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, result: "결과가 여기에 나타납니다")
+    socket =
+      assign(socket,
+        result: "결과가 여기에 나타납니다",
+        values: %{"number1" => nil, "number2" => nil},
+        errors: %{"number1" => [], "number2" => []}
+      )
+
     {:ok, socket}
   end
 
   def handle_event("input-change", %{"number1" => number1, "number2" => number2}, socket) do
-    case valid(number1, number2) do
-      {:ok, {n1, n2}} ->
+    case {valid_integer(number1), valid_integer(number2)} do
+      {{:ok, n1}, {:ok, n2}} ->
         result =
           ["+", "-", "*", "/"]
           |> Enum.map(fn x -> calc(x, n1, n2) end)
           |> Enum.map(fn x -> print(x) end)
           |> Enum.join("<br/>")
 
-        socket = assign(socket, result: result)
+        socket =
+          assign(socket,
+            result: result,
+            values: %{"number1" => number1, "number2" => number2},
+            errors: %{"number1" => [], "number2" => []}
+          )
+
         {:noreply, socket}
 
-      {:error, msg} ->
-        socket = assign(socket, result: msg)
+      {{:error, msg1}, {:error, msg2}} ->
+        socket =
+          assign(socket,
+            result: "결과가 여기에 나타납니다",
+            values: %{"number1" => number1, "number2" => number2},
+            errors: %{"number1" => [msg1], "number2" => [msg2]}
+          )
+
+        {:noreply, socket}
+
+      {{:error, msg}, _} ->
+        socket =
+          assign(socket,
+            result: "결과가 여기에 나타납니다",
+            values: %{"number1" => number1, "number2" => number2},
+            errors: %{"number1" => [msg], "number2" => []}
+          )
+
+        {:noreply, socket}
+
+      {_, {:error, msg}} ->
+        socket =
+          assign(socket,
+            result: "결과가 여기에 나타납니다",
+            values: %{"number1" => number1, "number2" => number2},
+            errors: %{"number1" => [], "number2" => [msg]}
+          )
+
         {:noreply, socket}
     end
   end
@@ -40,12 +78,14 @@ defmodule CodingTrainingElixirWeb.Chapter5Live do
     end
   end
 
-  def valid(number1, number2) do
-    with {int1, ""} <- Integer.parse(number1),
-         {int2, ""} <- Integer.parse(number2),
-         true <- int1 > 0,
-         true <- int2 > 0 do
-      {:ok, {int1, int2}}
+  def valid_integer("") do
+    {:error, "값이 입력되지 않았습니다."}
+  end
+
+  def valid_integer(number) do
+    with {int1, ""} <- Integer.parse(number),
+         true <- int1 > 0 do
+      {:ok, int1}
     else
       :error -> {:error, "정수가 아닌 값이 입력되었습니다."}
       _ -> {:error, "0 또는 음수가 입력되었습니다."}
