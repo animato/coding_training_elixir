3ae8e6fbb28893617607a1aa4cd41852c777e025
dsyang <dsyang.tech@gmail.com>
2024-09-17 17:43:40 +0900

    코드 구조 개선

--- lib/coding_training_elixir_web/live/chapter12/chapter12_live.ex
+++ lib/coding_training_elixir_web/live/chapter12/chapter12_live.ex
@@ -14,91 +14,68 @@ defmodule CodingTrainingElixirWeb.Chapter12Live do
     {:ok, socket}
   end
 
-  def handle_event(
-        "validate",
-        %{"interest" => interest, "principal" => principal, "years" => years},
-        socket
-      ) do
-    principal =
-      case Integer.parse(principal) do
-        {int, ""} when int > 0 -> %{value: principal, error: []}
-        {_, ""} -> %{value: principal, error: ["0 또는 음수 값이 입력되었습니다."]}
-        _ -> %{value: principal, error: ["숫자가 아닌 값이 입력되었습니다."]}
-      end
-
-    interest =
-      case Float.parse(interest) do
-        {float, ""} when float > 0 -> %{value: interest, error: []}
-        {_, ""} -> %{value: interest, error: ["0 또는 음수 값이 입력되었습니다."]}
-        _ -> %{value: interest, error: ["숫자가 아닌 값이 입력되었습니다."]}
-      end
-
-    years =
-      case Integer.parse(years) do
-        {int, ""} when int > 0 -> %{value: years, error: []}
-        {_, ""} -> %{value: years, error: ["0 또는 음수 값이 입력되었습니다."]}
-        _ -> %{value: years, error: ["숫자가 아닌 값이 입력되었습니다."]}
-      end
-
-    socket =
-      assign(socket,
-        valid: principal.error == [] and interest.error == [] and years.error == [],
-        principal: principal,
-        interest: interest,
-        years: years,
-        result: []
-      )
-
+  def handle_event("validate", params, socket) do
+    socket = update_values(socket, params)
     {:noreply, socket}
   end
 
-  def handle_event(
-        "input-submit",
-        %{"interest" => interest, "principal" => principal, "years" => years},
-        socket
-      ) do
-    principal =
-      case Integer.parse(principal) do
-        {int, ""} when int > 0 -> %{value: int, error: []}
-        {_, ""} -> %{value: principal, error: ["0 또는 음수 값이 입력되었습니다."]}
-        _ -> %{value: principal, error: ["숫자가 아닌 값이 입력되었습니다."]}
-      end
+  def handle_event("input-submit", params, socket) do
+    socket = update_values(socket, params)
 
-    interest =
-      case Float.parse(interest) do
-        {float, ""} when float > 0 -> %{value: float, error: []}
-        {_, ""} -> %{value: interest, error: ["0 또는 음수 값이 입력되었습니다."]}
-        _ -> %{value: interest, error: ["숫자가 아닌 값이 입력되었습니다."]}
-      end
+    case socket.assigns.valid do
+      true ->
+        result =
+          calculate_per_year(
+            socket.assigns.principal.value,
+            socket.assigns.interest.value,
+            socket.assigns.years.value
+          )
 
-    years =
-      case Integer.parse(years) do
-        {int, ""} when int > 0 -> %{value: int, error: []}
-        {_, ""} -> %{value: years, error: ["0 또는 음수 값이 입력되었습니다."]}
-        _ -> %{value: years, error: ["숫자가 아닌 값이 입력되었습니다."]}
-      end
+        {:noreply, assign(socket, result: result)}
 
-    result =
-      Enum.map(1..years.value, fn year ->
-        calculate =
-          calculateSimpleInterest(principal.value, interest.value, year) |> Float.ceil(2)
-
-        "연 #{interest.value}%, #{year}년 후 #{calculate}를 얻게 됩니다."
-      end)
-
-    socket =
-      assign(socket,
-        valid: false,
-        principal: principal,
-        interest: interest,
-        years: years,
-        result: result
-      )
-
-    {:noreply, socket}
+      false ->
+        {:noreply, socket}
+    end
   end
 
-  def calculateSimpleInterest(p, r, t) do
+  defp update_values(socket, %{"interest" => interest, "principal" => principal, "years" => years}) do
+    principal = validate_integer(principal)
+    interest = validate_float(interest)
+    years = validate_integer(years)
+
+    assign(socket,
+      valid: principal.error == [] and interest.error == [] and years.error == [],
+      principal: principal,
+      interest: interest,
+      years: years,
+      result: []
+    )
+  end
+
+  def validate_integer(string) do
+    case Integer.parse(string) do
+      {int, ""} when int > 0 -> %{value: int, error: []}
+      {_, ""} -> %{value: string, error: ["0 또는 음수 값이 입력되었습니다."]}
+      _ -> %{value: string, error: ["숫자가 아닌 값이 입력되었습니다."]}
+    end
+  end
+
+  def validate_float(string) do
+    case Float.parse(string) do
+      {float, ""} when float > 0 -> %{value: float, error: []}
+      {_, ""} -> %{value: string, error: ["0 또는 음수 값이 입력되었습니다."]}
+      _ -> %{value: string, error: ["숫자가 아닌 값이 입력되었습니다."]}
+    end
+  end
+
+  def calculate_simple_interest(p, r, t) do
     p * (1 + r / 100 * t)
+  end
+
+  def calculate_per_year(principal, interest, years) do
+    Enum.map(1..years, fn year ->
+      calculate = calculate_simple_interest(principal, interest, year) |> Float.ceil(2)
+      "연 #{interest}%, #{year}년 후 #{calculate}를 얻게 됩니다."
+    end)
   end
 end
