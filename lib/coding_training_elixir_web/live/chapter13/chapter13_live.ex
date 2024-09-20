bd0169381b1b47b1d19cae2533aa8162ad3f1fcd
dsyang <dsyang.tech@gmail.com>
2024-09-20 19:31:58 +0900

    연습문제13 원리금 입력하면 필요한 원금 계산

--- lib/coding_training_elixir_web/live/chapter13/chapter13_live.ex
+++ lib/coding_training_elixir_web/live/chapter13/chapter13_live.ex
@@ -4,31 +4,61 @@ defmodule CodingTrainingElixirWeb.Chapter13Live do
   def mount(_params, _session, socket) do
     socket =
       assign(socket,
-        valid: false,
-        principal: %{value: nil, error: []},
-        interest: %{value: nil, error: []},
-        years: %{value: nil, error: []},
-        compound: %{value: nil, error: []},
-        result: []
+        form1: %{
+          principal: %{value: nil, error: []},
+          interest: %{value: nil, error: []},
+          years: %{value: nil, error: []},
+          compound: %{value: nil, error: []},
+          valid: false
+        },
+        result1: [],
+        form2: %{
+          total: %{value: nil, error: []},
+          interest: %{value: nil, error: []},
+          years: %{value: nil, error: []},
+          compound: %{value: nil, error: []},
+          valid: false
+        },
+        result2: []
       )
 
     {:ok, socket}
   end
 
-  def handle_event("validate", params, socket) do
+  def handle_event("form1", params, socket) do
     socket = update_values(socket, params)
 
-    case socket.assigns.valid do
+    case socket.assigns.form1.valid do
       true ->
         result =
           calculate_per_year(
-            socket.assigns.principal.value,
-            socket.assigns.interest.value,
-            socket.assigns.years.value,
-            socket.assigns.compound.value
+            socket.assigns.form1.principal.value,
+            socket.assigns.form1.interest.value,
+            socket.assigns.form1.years.value,
+            socket.assigns.form1.compound.value
           )
 
-        {:noreply, assign(socket, result: result)}
+        {:noreply, assign(socket, result1: result)}
+
+      false ->
+        {:noreply, socket}
+    end
+  end
+
+  def handle_event("form2", params, socket) do
+    socket = update_values2(socket, params)
+
+    case socket.assigns.form2.valid do
+      true ->
+        result =
+          calculate_principal(
+            socket.assigns.form2.total.value,
+            socket.assigns.form2.interest.value,
+            socket.assigns.form2.years.value,
+            socket.assigns.form2.compound.value
+          )
+
+        {:noreply, assign(socket, result2: result)}
 
       false ->
         {:noreply, socket}
@@ -51,12 +81,39 @@ defmodule CodingTrainingElixirWeb.Chapter13Live do
       |> Enum.all?(fn list -> list == [] end)
 
     assign(socket,
-      valid: valid,
-      principal: principal,
-      interest: interest,
-      years: years,
-      compound: compound,
-      result: []
+      form1: %{
+        valid: valid,
+        principal: principal,
+        interest: interest,
+        years: years,
+        compound: compound
+      }
+    )
+  end
+
+  defp update_values2(socket, %{
+         "interest" => interest,
+         "total" => total,
+         "years" => years,
+         "compound" => compound
+       }) do
+    total = validate_integer(total)
+    interest = validate_float(interest)
+    years = validate_integer(years)
+    compound = validate_integer(compound)
+
+    valid =
+      [total.error, interest.error, years.error, compound.error]
+      |> Enum.all?(fn list -> list == [] end)
+
+    assign(socket,
+      form2: %{
+        valid: valid,
+        total: total,
+        interest: interest,
+        years: years,
+        compound: compound
+      }
     )
   end
 
@@ -87,5 +144,9 @@ defmodule CodingTrainingElixirWeb.Chapter13Live do
 
       "연 #{interest}%, #{year}년 후 #{calculate}를 얻게 됩니다."
     end)
+  end
+
+  def calculate_principal(a, r, t, n) do
+    a / :math.pow(1 + r / 100 / n, n * t)
   end
 end
