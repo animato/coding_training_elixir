6fbe9c75beffa10532da11497d7408ef96ad938d
dsyang <dsyang.tech@gmail.com>
2024-10-01 22:50:12 +0900

    연습문제16 입력 값 검증 추가

--- lib/coding_training_elixir_web/live/chapter16/chapter16_live.ex
+++ lib/coding_training_elixir_web/live/chapter16/chapter16_live.ex
@@ -11,9 +11,15 @@ defmodule CodingTrainingElixirWeb.Chapter16Live do
   end
 
   def handle_event("validate", %{"age" => age}, socket) do
+    errors =
+      case validate_integer(age) do
+        {:ok, _} -> []
+        {:error, message} -> [age: {message, []}]
+      end
+
     {:noreply,
      assign(socket,
-       form: to_form(%{"age" => age}, errors: [])
+       form: to_form(%{"age" => age}, errors: errors)
      )}
   end
 
@@ -31,5 +37,13 @@ defmodule CodingTrainingElixirWeb.Chapter16Live do
      socket
      |> assign(form: to_form(%{"age" => nil}, errors: []))
      |> put_flash(result, message)}
+  end
+
+  def validate_integer(string) do
+    case Integer.parse(string) do
+      {int, ""} when int > 0 -> {:ok, int}
+      {_, ""} -> {:error, "0 또는 음수 값이 입력되었습니다."}
+      _ -> {:error, "숫자가 아닌 값이 입력되었습니다."}
+    end
   end
 end
