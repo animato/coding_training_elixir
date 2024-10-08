a2b50175af3163fd530cc2f52e12b73bb8a098e1
dsyang <dsyang.tech@gmail.com>
2024-10-08 21:34:29 +0900

    연습문제18 구조 개선

--- lib/coding_training_elixir_web/live/chapter18/chapter18_live.ex
+++ lib/coding_training_elixir_web/live/chapter18/chapter18_live.ex
@@ -24,11 +24,31 @@ defmodule CodingTrainingElixirWeb.Chapter18Live do
 
   def handle_event(
         "validate",
-        %{"temperature" => temperature, "origin" => origin, "target" => target},
+        %{"temperature" => temperature, "origin" => origin, "target" => target} = params,
         socket
       ) do
+    case validate_integer(temperature) do
+      {:ok, temperature} ->
+        target_label = Map.get(@reverse_options, target)
+
+        result =
+          convert_temperature_unit(temperature, origin, target)
+          |> generate_result_message(target_label)
+
+        {:noreply, update_socket(socket, result, [], params)}
+
+      {:error, message} ->
+        {:noreply, update_socket(socket, "", [temperature: {message, []}], params)}
+    end
+  end
+
+  def update_socket(
+        socket,
+        result,
+        errors,
+        %{"temperature" => _, "origin" => origin, "target" => target} = params
+      ) do
     origin_label = Map.get(@reverse_options, origin)
-    target_label = Map.get(@reverse_options, target)
 
     filtered_origin =
       Map.delete(@reverse_options, target) |> Map.new(fn {k, v} -> {v, k} end)
@@ -36,45 +56,13 @@ defmodule CodingTrainingElixirWeb.Chapter18Live do
     filtered_target =
       Map.delete(@reverse_options, origin) |> Map.new(fn {k, v} -> {v, k} end)
 
-    case validate_integer(temperature) do
-      {:ok, temperature} ->
-        result =
-          case {origin, target} do
-            {"F", "C"} -> fahrenheit_to_celsius(temperature)
-            {"F", "K"} -> fahrenheit_to_celsius(temperature) |> celsius_to_kelvin()
-            {"C", "F"} -> celsius_to_fahrenheit(temperature)
-            {"C", "K"} -> celsius_to_kelvin(temperature)
-            {"K", "F"} -> kelvin_to_celsius(temperature) |> celsius_to_fahrenheit()
-            {"K", "C"} -> kelvin_to_celsius(temperature)
-            _ -> ""
-          end
-
-        {:noreply,
-         assign(socket,
-           result: generate_result_message(result, target_label),
-           origin_label: origin_label,
-           origin_options: filtered_origin,
-           target_options: filtered_target,
-           form:
-             to_form(%{"temperature" => temperature, "origin" => origin, "target" => target},
-               errors: []
-             )
-         )}
-
-      {:error, message} ->
-        {:noreply,
-         assign(socket,
-           result: "",
-           origin: origin,
-           origin_label: origin_label,
-           origin_options: filtered_origin,
-           target_options: filtered_target,
-           form:
-             to_form(%{"temperature" => temperature, "origin" => origin, "target" => target},
-               errors: [temperature: {message, []}]
-             )
-         )}
-    end
+    assign(socket,
+      result: result,
+      origin_label: origin_label,
+      origin_options: filtered_origin,
+      target_options: filtered_target,
+      form: to_form(params, errors: errors)
+    )
   end
 
   defp generate_result_message("", _), do: ""
@@ -83,11 +71,31 @@ defmodule CodingTrainingElixirWeb.Chapter18Live do
     "변환된 #{target_label} 온도는 #{result} 입니다."
   end
 
+  def convert_temperature_unit(temperature, origin, target) do
+    case {origin, target} do
+      {"F", "C"} -> fahrenheit_to_celsius(temperature)
+      {"F", "K"} -> fahrenheit_to_kelvin(temperature)
+      {"C", "F"} -> celsius_to_fahrenheit(temperature)
+      {"C", "K"} -> celsius_to_kelvin(temperature)
+      {"K", "F"} -> kelvin_to_fahrenheit(temperature)
+      {"K", "C"} -> kelvin_to_celsius(temperature)
+      _ -> ""
+    end
+  end
+
   def validate_integer(string) do
     case Integer.parse(string) do
       {int, ""} -> {:ok, int}
       _ -> {:error, "숫자가 아닌 값이 입력되었습니다."}
     end
+  end
+
+  def fahrenheit_to_kelvin(f) do
+    fahrenheit_to_celsius(f) |> celsius_to_kelvin()
+  end
+
+  def kelvin_to_fahrenheit(k) do
+    kelvin_to_celsius(k) |> celsius_to_fahrenheit()
   end
 
   def fahrenheit_to_celsius(f) do
