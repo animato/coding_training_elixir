6dae7d7025e095f7e6a652e5511e8d886db786a7
dsyang <dsyang.tech@gmail.com>
2024-10-22 20:42:30 +0900

    연습문제21 월 변환기 case로 구현

--- lib/coding_training_elixir_web/live/chapter21/chapter21_live.ex
+++ lib/coding_training_elixir_web/live/chapter21/chapter21_live.ex
@@ -4,7 +4,8 @@ defmodule CodingTrainingElixirWeb.Chapter21Live do
   def mount(_params, _session, socket) do
     socket =
       assign(socket,
-        form: to_form(%{}, errors: [])
+        form: to_form(%{}, errors: []),
+        result: nil
       )
 
     {:ok, socket}
@@ -12,12 +13,33 @@ defmodule CodingTrainingElixirWeb.Chapter21Live do
 
   def handle_event(
         "change",
-        params,
+        %{"monthNumber" => month_number} = params,
         socket
       ) do
+    month_text = get_month_text(month_number)
+
     {:noreply,
      assign(socket,
-       form: to_form(params, errors: [])
+       form: to_form(params, errors: []),
+       result: month_text
      )}
+  end
+
+  def get_month_text(month_number) do
+    case month_number do
+      "1" -> "1월"
+      "2" -> "2월"
+      "3" -> "3월"
+      "4" -> "4월"
+      "5" -> "5월"
+      "6" -> "6월"
+      "7" -> "7월"
+      "8" -> "8월"
+      "9" -> "9월"
+      "10" -> "10월"
+      "11" -> "11월"
+      "12" -> "12월"
+      _ -> nil
+    end
   end
 end
