ff41058acc0b5cec7cb8fcd8abfb71f548dc6e55
dsyang <dsyang.tech@gmail.com>
2024-10-23 21:16:36 +0900

    연습문제21 월 변환기 case를 Map으로 대체

--- lib/coding_training_elixir_web/live/chapter21/chapter21_live.ex
+++ lib/coding_training_elixir_web/live/chapter21/chapter21_live.ex
@@ -1,6 +1,20 @@
 defmodule CodingTrainingElixirWeb.Chapter21Live do
   use CodingTrainingElixirWeb, :live_view
 
+  @month_map %{
+    "1" => "1월",
+    "2" => "2월",
+    "3" => "3월",
+    "4" => "4월",
+    "5" => "5월",
+    "6" => "6월",
+    "7" => "7월",
+    "8" => "8월",
+    "9" => "9월",
+    "10" => "10월",
+    "11" => "11월",
+    "12" => "12월"
+  }
   def mount(_params, _session, socket) do
     socket =
       assign(socket,
@@ -26,20 +40,6 @@ defmodule CodingTrainingElixirWeb.Chapter21Live do
   end
 
   def get_month_text(month_number) do
-    case month_number do
-      "1" -> "1월"
-      "2" -> "2월"
-      "3" -> "3월"
-      "4" -> "4월"
-      "5" -> "5월"
-      "6" -> "6월"
-      "7" -> "7월"
-      "8" -> "8월"
-      "9" -> "9월"
-      "10" -> "10월"
-      "11" -> "11월"
-      "12" -> "12월"
-      _ -> nil
-    end
+    Map.get(@month_map, month_number)
   end
 end
