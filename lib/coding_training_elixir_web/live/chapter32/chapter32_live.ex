77a7692cf0104c153ec2904280cf646a62cc4e00
dsyang <dsyang.tech@gmail.com>
2024-11-11 21:20:47 +0900

    연습문제32 숫자 맞추기 게임 시작 데이터 구현

--- lib/coding_training_elixir_web/live/chapter32/chapter32_live.ex
+++ lib/coding_training_elixir_web/live/chapter32/chapter32_live.ex
@@ -8,10 +8,31 @@ defmodule CodingTrainingElixirWeb.Chapter32Live do
     socket =
       assign(socket,
         form: to_form(%{}, errors: []),
-        result: nil
+        result: nil,
+        game_active: false,
+        attempts: 0,
+        timer: 0
       )
 
     {:ok, socket}
+  end
+
+  def handle_event("start", %{"max" => max} = params, socket) do
+    max_number = String.to_integer(max)
+    secret_number = :rand.uniform(max_number)
+
+    IO.inspect(max_number)
+    IO.inspect(secret_number)
+
+    {:noreply,
+     assign(socket,
+       form: to_form(params, errors: []),
+       result: nil,
+       secret_number: secret_number,
+       game_active: true,
+       attempts: 0,
+       timer: 0
+     )}
   end
 
   def handle_event("submit", %{} = params, socket) do
