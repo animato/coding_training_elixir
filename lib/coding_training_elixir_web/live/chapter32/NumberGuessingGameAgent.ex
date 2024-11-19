87ab4aedc61684202e9a732902356732abe60cec
dsyang <dsyang.tech@gmail.com>
2024-11-19 21:03:57 +0900

    연습문제32 숫자 맞추기 게임 에이전트에서 타이머 분리

--- lib/coding_training_elixir_web/live/chapter32/NumberGuessingGameAgent.ex
+++ lib/coding_training_elixir_web/live/chapter32/NumberGuessingGameAgent.ex
@@ -6,35 +6,27 @@ defmodule CodingTrainingElixirWeb.NumberGuessingGameAgent do
   @doc """
   숫자 맞추기 게임 시작
   """
-  def start_game(user_id, max_number, tick_callback) do
-    {:ok, timer_ref} = :timer.send_interval(1000, tick_callback, :tick)
+  def start_game(user_id, max_number) do
+    Agent.start_link(
+      fn -> initial_state(max_number) end,
+      name: user_id
+    )
+  end
 
-    case Agent.start_link(
-           fn ->
-             %{
-               max_number: max_number,
-               secret_number: :rand.uniform(max_number),
-               previous_guesses: [],
-               attempts: 0,
-               timer_ref: timer_ref
-             }
-           end,
-           name: user_id
-         ) do
-      {:ok, _} ->
-        {:ok, %{result: nil, game_active: true, previous_guesses: [], attempts: 0}}
-
-      _ ->
-        {:error, %{result: nil, game_active: false, previous_guesses: [], attempts: 0}}
-    end
+  defp initial_state(max_number) do
+    %{
+      max_number: max_number,
+      secret_number: :rand.uniform(max_number),
+      previous_guesses: [],
+      attempts: 0
+    }
   end
 
   def guess_number(user_id, guess) do
     %{
       secret_number: secret_number,
       previous_guesses: previous_guesses,
-      attempts: attempts,
-      timer_ref: timer_ref
+      attempts: attempts
     } =
       Agent.get(user_id, & &1)
 
@@ -47,20 +39,13 @@ defmodule CodingTrainingElixirWeb.NumberGuessingGameAgent do
     {result, game_active} = check_guess(secret_number, String.to_integer(guess))
 
     if not game_active do
-      :timer.cancel(timer_ref)
-      Agent.stop(user_id, :normal)
+      reset(user_id)
     end
 
     %{result: result, game_active: game_active, previous_guesses: new, attempts: attempts + 1}
   end
 
   def reset(user_id) do
-    %{
-      timer_ref: timer_ref
-    } =
-      Agent.get(user_id, & &1)
-
-    :timer.cancel(timer_ref)
     Agent.stop(user_id, :normal)
   end
 
--- lib/coding_training_elixir_web/live/chapter32/chapter32_live.ex
+++ lib/coding_training_elixir_web/live/chapter32/chapter32_live.ex
@@ -14,6 +14,7 @@ defmodule CodingTrainingElixirWeb.Chapter32Live do
         game_active: false,
         previous_guesses: [],
         timer: 0,
+        timer_ref: nil,
         user_id: String.to_atom(:crypto.strong_rand_bytes(8) |> Base.encode16(case: :lower))
       )
 
@@ -23,24 +24,35 @@ defmodule CodingTrainingElixirWeb.Chapter32Live do
   def handle_event("start", %{"max" => max} = params, socket) do
     max_number = String.to_integer(max)
 
-    {:ok, result_map} =
-      NumberGuessingGameAgent.start_game(socket.assigns.user_id, max_number, self())
+    {start, _} =
+      NumberGuessingGameAgent.start_game(socket.assigns.user_id, max_number)
 
-    {:noreply,
-     assign(socket,
-       form: to_form(params, errors: []),
-       max_number: max,
-       result: result_map.result,
-       game_active: result_map.game_active,
-       game_mode: "mode",
-       previous_guesses: result_map.previous_guesses,
-       attempts: result_map.attempts
-     )}
+    if start == :ok do
+      {:ok, timer_ref} = :timer.send_interval(1000, self(), :tick)
+
+      {:noreply,
+       assign(socket,
+         form: to_form(params, errors: []),
+         max_number: max,
+         result: nil,
+         game_active: true,
+         previous_guesses: [],
+         attempts: 0,
+         timer_ref: timer_ref
+       )}
+    else
+      {:noreply,
+       assign(socket,
+         form: to_form(params, errors: [])
+       )}
+    end
   end
 
   def handle_event("submit", %{"guess" => guess} = params, socket) do
     result_map = NumberGuessingGameAgent.guess_number(socket.assigns.user_id, guess)
 
+    if not result_map.game_active, do: cancel_timer(socket.assigns.timer_ref)
+
     {:noreply,
      assign(socket,
        form: to_form(params, errors: []),
@@ -51,8 +63,10 @@ defmodule CodingTrainingElixirWeb.Chapter32Live do
      )}
   end
 
-  def handle_event("reset", params, socket) do
+  def handle_event("reset", _, socket) do
     NumberGuessingGameAgent.reset(socket.assigns.user_id)
+
+    cancel_timer(socket.assigns.timer_ref)
 
     {:noreply,
      assign(socket,
@@ -68,4 +82,16 @@ defmodule CodingTrainingElixirWeb.Chapter32Live do
   def handle_info(:tick, socket) do
     {:noreply, update(socket, :timer, fn timer -> timer + 1 end)}
   end
+
+  def terminate(reason, socket) do
+    # 사용자의 Agent를 종료
+
+    IO.inspect(reason)
+    NumberGuessingGameAgent.reset(socket.assigns.user_id)
+    cancel_timer(socket.assigns.timer_ref)
+    :ok
+  end
+
+  defp cancel_timer(timer_ref) when timer_ref != nil, do: :timer.cancel(timer_ref)
+  defp cancel_timer(nil), do: nil
 end
